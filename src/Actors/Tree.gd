9e5800890273479943c3b65b15eccf5bfef351ff
Vũ Cao Tân <60535960+username898@users.noreply.github.com>
2020-06-03 19:38:10 +0700

    'connect-to-next-screen'

--- src/Actors/Tree.gd
+++ src/Actors/Tree.gd
@@ -1,15 +1,6 @@
 extends Actor
-
-
-onready var stomp_area: Area2D = $StompArea2D
-
 export var score: = 100
 
-
-func _on_StompArea2D_area_entered(area: Area2D) -> void:
-	if area.global_position.y > stomp_area.global_position.y:
-		return
-	die()
 
 
 func die() -> void:
